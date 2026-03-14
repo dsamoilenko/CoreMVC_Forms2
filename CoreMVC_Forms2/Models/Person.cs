403a999c6a2b5b1cedcfbc1675c3abbcdcd07012
dsamoilenko <dsamoilenko@yahoo.com>
2026-03-14 16:25:41 +0300

    Person added

---
+++ CoreMVC_Forms2/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace CoreMVC_Forms2.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Agw { get; set; }
+
+        public int Weight { get; set; }
+
+        public Person()
+        {
+                
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
