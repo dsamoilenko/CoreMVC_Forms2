29392679a38c97e1bf9c8471362be4c4f8c8a124
dsamoilenko <dsamoilenko@yahoo.com>
2026-03-17 19:30:28 +0300

    Person changed

--- CoreMVC_Forms2/Models/Person.cs
+++ CoreMVC_Forms2/Models/Person.cs
@@ -6,15 +6,18 @@
 
         public string Address { get; set; }
 
-        public int Age { get; set; }
+        public decimal Age { get; set; }
 
-        public decimal Weight { get; set; }
+        public decimal Weight { get; }
 
-        public decimal Height { get; set; }
+        public decimal Height { get; }
 
-        public Person(string name, string address, int age, int weight)
+        public string LastName { get; set; }
+
+        public Person(string name, string lastName, string address, int age, int weight)
         {
             Name = name;
+            LastName = lastName;
             Address = address;
             Age = age;
             Weight = weight;
